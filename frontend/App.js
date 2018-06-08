68e4c7055f071f477bda4d3e4f6484ddca7dc42c
benche <ben.che9@gmail.com>
2018-06-08 19:58:58 -0400

    imported native packages

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>List all your leftover ingredients here!</Text>
+        <Text>ex: chicken,bread,juice,jellybeans</Text>
+        <FormLabel>Name</FormLabel>
+        <FormInput />
+        <FormValidationMessage>Error message</FormValidationMessage>
+        <FormValidationMessage>{'This field is required'}</FormValidationMessage>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );

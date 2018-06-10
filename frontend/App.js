27a96d88bee70c02caee5d0fd9f11d450853b4e7
benche <ben.che9@gmail.com>
2018-06-09 20:49:20 -0400

    created controlled input field in react native and importing axios

--- frontend/App.js
+++ frontend/App.js
@@ -3,15 +3,26 @@ import { StyleSheet, Text, View } from 'react-native';
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: ''
+    }
+  }
+
+  handleInput = (text) => {
+    this.setState = ({
+      inputText : text
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>List all your leftover ingredients here!</Text>
         <Text>ex: chicken,bread,juice,jellybeans</Text>
-        <FormLabel>Name</FormLabel>
-        <FormInput />
-        <FormValidationMessage>Error message</FormValidationMessage>
-        <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+        <FormLabel>Seach</FormLabel>
+        <FormInput onChangeText={(text) => this.handleInput(text)}/>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
